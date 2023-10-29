f585fc66493c81457876f62c73be2daee88fb810
Christy <christy@zipcoders-mbp-6.mynetworksettings.com>
2023-10-29 09:16:14 -0400

    need more change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
     }
+
+    Scanner scanner = new Scanner(System.in);
+     System.out.println("Enter your name? ");
+     String name = scanner.nextLine();
+     System.out.println( "Hello my name is" + name);
+
+
+
 }
+
+
+
+
+
+
+
