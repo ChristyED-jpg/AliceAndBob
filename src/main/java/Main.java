48066f53014f17b48f35ca2159446c4e22c850a8
Christy <christy@zipcoders-mbp-6.mynetworksettings.com>
2023-10-29 09:24:09 -0400

    changes made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-    }
 
-    Scanner scanner = new Scanner(System.in);
-     System.out.println("Enter your name? ");
-     String name = scanner.nextLine();
-     System.out.println( "Hello my name is" + name);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name? ");
+        String  name = scanner.nextLine();
+        System.out.println( "Hello my name is " + name);
+
+    }
 
 
 
